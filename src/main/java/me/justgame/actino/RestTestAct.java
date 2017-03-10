1b153226c35e34137b1498188e5d57f1e8aeaf08
justgame <289243011@qq.com>
2017-03-10 15:48:20 +0800

    update

--- src/main/java/me/justgame/actino/RestTestAct.java
+++ src/main/java/me/justgame/actino/RestTestAct.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -14,7 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class RestTestAct {
 
 
-    @RequestMapping(method = RequestMethod.GET, value = "/user/{id}")
+    @RequestMapping(method = RequestMethod.GET, value = "/user/{id}.do")
     public ModelAndView getUser(@PathVariable("id") String id) throws Exception {
         User user = new User();
         user.setId(id);
@@ -23,5 +24,25 @@ public class RestTestAct {
         ModelAndView modelAndView = new ModelAndView("all/rest_test_page");
         modelAndView.addObject("user", user);
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/takeResult/{result}.do", method = RequestMethod.GET, produces = "text/html")
+    @ResponseBody public String getResult(@PathVariable("result") String result) throws Exception {
+        return  result + " ,method : get";
+    }
+
+    @RequestMapping(value = "/takeResult/{result}.do", method = RequestMethod.POST)
+    @ResponseBody public String postResult(@PathVariable("result") String result) throws Exception {
+        return  result + " ,method : post";
+    }
+
+    @RequestMapping(value = "/takeResult/{result}.do", method = RequestMethod.PUT)
+    @ResponseBody public String putResult(@PathVariable("result") String result) throws Exception {
+        return  result + " ,method : put";
+    }
+
+    @RequestMapping(value = "/takeResult/{result}.do", method = RequestMethod.DELETE)
+    @ResponseBody public String deleteResult(@PathVariable("result") String result) throws Exception {
+        return  result + " ,method : delete";
     }
 }
